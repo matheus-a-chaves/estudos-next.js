47efd94ce507c409efd2c031ce8a76f2951ed544
Matheus Alves Chaves <matheuschaves@ciandt.com>
2025-09-02 19:32:49 -0300

    adding estilos

---
+++ src/app/components/MyComponent.jsx
@@ -0,0 +1,10 @@
+
+const MyComponent = () => {
+    return (
+        <div className="p-4 bg-blue-500 text-white text-lg font-bold">
+            Next com Tailwind
+        </div>
+    )
+}
+
+export default MyComponent
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,19 @@
+import MyComponent from './components/MyComponent';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1 >Titulo do projeto</h1>
+
+      <h2 className={styles.heading_module}>Titulo do projeto H2</h2>
+      <div className={styles.container}>
+        <p>Texto herdado</p>
+      </div>
+      <div className={styles.container}>
+        <h1>Texto sem herança</h1>
+      </div>
+      <MyComponent />
     </div>
   );
 }
